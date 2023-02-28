d61ad474424706ad0741fbd808178e2fbbb5832f
Johan Andersson <johanbkandersson@gmail.com>
2023-02-28 10:29:09 +0100

    Added scrollbars to text editor and preview

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -42,9 +42,10 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(197, 164);
+            this.btnSave.Location = new System.Drawing.Point(263, 202);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.Size = new System.Drawing.Size(100, 28);
             this.btnSave.TabIndex = 0;
             this.btnSave.Text = "Logga!";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -52,10 +53,13 @@
             // 
             // txtBoxLog
             // 
-            this.txtBoxLog.Location = new System.Drawing.Point(12, 12);
+            this.txtBoxLog.Font = new System.Drawing.Font("Gentium Book Basic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxLog.Location = new System.Drawing.Point(16, 15);
+            this.txtBoxLog.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtBoxLog.Multiline = true;
             this.txtBoxLog.Name = "txtBoxLog";
-            this.txtBoxLog.Size = new System.Drawing.Size(260, 146);
+            this.txtBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBoxLog.Size = new System.Drawing.Size(345, 179);
             this.txtBoxLog.TabIndex = 1;
             this.txtBoxLog.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -63,42 +67,47 @@
             // 
             this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 238);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 293);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(179, 95);
+            this.listBox1.Size = new System.Drawing.Size(237, 116);
             this.listBox1.TabIndex = 2;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.kopieraToolStripMenuItem,
             this.taBortToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(171, 48);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(202, 52);
             this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
             // 
             // kopieraToolStripMenuItem
             // 
             this.kopieraToolStripMenuItem.Name = "kopieraToolStripMenuItem";
-            this.kopieraToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.kopieraToolStripMenuItem.Size = new System.Drawing.Size(201, 24);
             this.kopieraToolStripMenuItem.Text = "Kopiera till urklipp";
             this.kopieraToolStripMenuItem.Click += new System.EventHandler(this.kopieraToolStripMenuItem_Click);
             // 
             // taBortToolStripMenuItem
             // 
             this.taBortToolStripMenuItem.Name = "taBortToolStripMenuItem";
-            this.taBortToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.taBortToolStripMenuItem.Size = new System.Drawing.Size(201, 24);
             this.taBortToolStripMenuItem.Text = "Ta bort";
             this.taBortToolStripMenuItem.Click += new System.EventHandler(this.taBortToolStripMenuItem_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(198, 238);
+            this.textBox1.Location = new System.Drawing.Point(264, 293);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(224, 95);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(297, 116);
             this.textBox1.TabIndex = 3;
             // 
             // comboBox1
@@ -109,22 +118,24 @@
             "Idag ",
             "Igår",
             "3 dagar sen"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 350);
+            this.comboBox1.Location = new System.Drawing.Point(16, 431);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
             this.comboBox1.TabIndex = 7;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(438, 399);
+            this.ClientSize = new System.Drawing.Size(584, 491);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.txtBoxLog);
             this.Controls.Add(this.btnSave);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Loggare";
             this.contextMenuStrip1.ResumeLayout(false);
